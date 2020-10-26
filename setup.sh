6e5633d827348df89c54c3e46cfdb0a80f7816b9
Patrick Haußmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-10-26 23:35:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ apt install sudo
 usermod -aG sudo patrick
 
 # install basic tools
-apt install -y mosh sudo neovim tmux unattended-upgrades fail2ban htop curl wget ufw stow git
+apt install -y mosh sudo vim tmux unattended-upgrades fail2ban htop curl wget ufw stow git httpie
 apt autoremove -y
 
 # ssh settings
